97778cc342125743a046d3df3dff06219aa3c947
Harish Kumar <harishdevelopment@users.noreply.github.com>
2021-02-12 00:27:06 +0000

    Created empty spring boot project with camel

---
+++ src/main/java/com/hkdemo/camelgraaljsdemo/CamelGraalJsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hkdemo.camelgraaljsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelGraalJsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamelGraalJsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hkdemo/camelgraaljsdemo/CamelGraalJsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hkdemo.camelgraaljsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelGraalJsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
